2b64c8f466edb7fb0ddf0435990b3ab3275dcb28
Merlintor <merlinfuchs2001@gmail.com>
2021-03-30 21:03:02 +0200

    increase response delay even more *sigh*

--- dbots/cmd/command.py
+++ dbots/cmd/command.py
@@ -298,7 +298,7 @@ class CommandContext:
 
         else:
             self.future.set_result(response)
-            await asyncio.sleep(0.5)
+            await asyncio.sleep(1)
 
     def respond(self, *args, **kwargs):
         return self.respond_with(InteractionResponse.message(*args, **kwargs))
